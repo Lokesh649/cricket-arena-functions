242fde8339f99bd521475870a5f1c683475ab663
Lokesh <lokesh.official211@gmail.com>
2023-10-07 15:28:23 +0530

    Initial Commit

---
+++ src/main/java/com/game/CricketArenaFunctionsApplication.java
@@ -0,0 +1,13 @@
+package com.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketArenaFunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketArenaFunctionsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/game/util/JfrogTest.java
@@ -0,0 +1,7 @@
+package com.game.util;
+
+public class JfrogTest {
+	public void letsTest() {
+		System.out.println("I'm Invoked");
+	}
+}
